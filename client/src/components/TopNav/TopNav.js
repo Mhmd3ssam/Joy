0e240a9da974edb0f21fc3729beea4797004ce08
RehamMuhammad <Reham.Muhammad.helmy@gmail.com>
2021-11-02 12:37:56 +0200

    Add Dropdown menus to Navbar

--- client/src/components/TopNav/TopNav.js
+++ client/src/components/TopNav/TopNav.js
@@ -1,7 +1,47 @@
 import React from "react";
 import "./topnav.css";
-import Dropdown from '../Dropdown/Dropdown'
+import Dropdown from '../Dropdown/Dropdown';
+import notifications from '../../assets/JsonData/notification.json';
+import { Link } from "react-router-dom";
+import user_image from '../../assets/images/girlavatar.jpg'
 
+import user_menu from '../../assets/JsonData/user_menus.json'
+
+
+
+const curr_user = {
+    display_name: 'Reham Muhammad',
+    image: user_image
+}
+
+const renderNotificationItem = (item, index) => (
+    <div className="notification-item" key={index}>
+        <i className={item.icon}></i>
+        <span>{item.content}</span>
+    </div>
+)
+
+
+const renderUserToggle = (user) => (
+    <div className="topnav__right-user">
+        <div className="topnav__right-user__image">
+            <img src={user.image} alt="" />
+        </div>
+        <div className="topnav__right-user__name">
+            {user.display_name}
+        </div>
+    </div>
+)
+
+
+const renderUserMenu =(item, index) => (
+    <Link to='/' key={index}>
+        <div className="notification-item">
+            <i className={item.icon}></i>
+            <span>{item.content}</span>
+        </div>
+    </Link>
+)
 function TopNav() {
   return (
     <div className="topnav">
@@ -12,20 +52,21 @@ function TopNav() {
 
       <div className="topnav__right">
         <div className="topnav__right-item">
-          {/* <Dropdown
+          <Dropdown
                         customToggle={() => renderUserToggle(curr_user)}
                         contentData={user_menu}
                         renderItems={(item, index) => renderUserMenu(item, index)}
-                    /> */}
+                    />
         </div>
         <div className="topnav__right-item">
-          {/* <Dropdown
+          { <Dropdown
                         icon='bx bx-bell'
                         badge='12'
                         contentData={notifications}
                         renderItems={(item, index) => renderNotificationItem(item, index)}
                         renderFooter={() => <Link to='/'>View All</Link>}
-                    /> */}
+                        />
+          }                   
           <div className="topnav__right-item">{/* <ThemeMenu/> */}</div>{" "}
         </div>
       </div>
