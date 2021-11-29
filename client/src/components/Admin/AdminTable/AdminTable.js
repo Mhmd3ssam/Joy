443e4ec2ca8abc4798bf7c359ae77f15f5f66e28
RehamMuhammad <Reham.Muhammad.helmy@gmail.com>
2021-11-29 18:17:43 +0200

    Edit User Table Functionality & UI

--- client/src/components/Admin/AdminTable/AdminTable.js
+++ client/src/components/Admin/AdminTable/AdminTable.js
@@ -153,7 +153,7 @@ function AdminTable() {
                       <th scope="col">Paid Status</th>
                       <th scope="col">Payment button</th>
                       <th scope="col">Subcription Plan</th>
-                      <th scope="col">Delete User</th>
+                      <th scope="col">Creating Service</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -214,21 +214,14 @@ function AdminTable() {
                                 deleteUser(user);
                               }}
                               type="button"
-                              class="btn btn-danger btn-sm px-3 mb-2 ms-1 material-tooltip-main"
+                              class={`badge bg-${
+                                user.displayServ ? "danger" : "primary"
+                              } btn-sm  px-3 mb-2 ms-1 material-tooltip-main`}
                               data-toggle="tooltip"
                               data-placement="top"
                               title="Add to wishlist"
                             >
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                fill="currentColor"
-                                class="bi bi-trash-fill"
-                                viewBox="0 0 16 16"
-                              >
-                                <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
-                              </svg>
+                              {user.displayServ? "Prevent User" : "Enable User"}
                             </button>
                           </td>
                         </tr>
